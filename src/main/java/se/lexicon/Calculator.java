b3bccefd4347cb7cb02655ee72f9734ff4a25533
leoodindealcantara <leoalcantara@icloud.com>
2021-05-27 12:25:44 +0200

    Calculator

--- src/main/java/se/lexicon/Calculator.java
+++ src/main/java/se/lexicon/Calculator.java
@@ -4,56 +4,66 @@ import java.util.Scanner;
 
 public class Calculator {
 
+    public static double addition(double num1, double num2) {
+        return num1 + num2;
+    }
 
-        public static void main(String[] args) {
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
 
-            boolean calculate = true;
-            while (calculate) {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Please enter your first number.");
-                double num1 = scanner.nextDouble();
-                System.out.println("Please enter your operator.");
-                char operator = scanner.next().charAt(0);
-                System.out.println("Please enter your second number.");
-                double num2 = scanner.nextDouble();
-                double result = 0;
+    public static double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
 
-                switch (operator) {
-                    case '+':
-                        result = num1 + num2;
-                        break;
-                    case '-':
-                        result = num1 - num2;
-                        break;
-                    case '/':
-                        result = num1 / num2;
-                        break;
-                    case '*':
-                        result = num1 * num2;
-                        break;
-                    default:
-                        System.out.println(operator + " is not supported.");
-                }
+    public static double division(double num1, double num2) {
+        return num1 / num2;
+    }
 
-                System.out.println("Your calculation equals: " + result);
+    public static void main(String[] args) {
 
-                System.out.println("Do you want to continue (y/n)");
-                char test = scanner.next().charAt(0);
+        boolean calculate = true;
+        while (calculate) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter your first number.");
+            double num1 = scanner.nextDouble();
+            System.out.println("Please enter your operator: +, -, *, or / .");
+            char operator = scanner.next().charAt(0);
+            System.out.println("Please enter your second number.");
+            double num2 = scanner.nextDouble();
+            double result = 0;
 
-                if (test == 'n'){
-                    calculate = false;
-                } else if (test == 'y') {
-                    calculate = true;
-                } else {
-                    System.out.println("Your choice is not a valid. Please choose (y/n).");
-                }
+            switch (operator) {
+                case '+':
+                    result = addition(num1, num2);
+                    break;
+                case '-':
+                    result = subtraction(num1, num2);
+                    break;
+                case '/':
+                    result = division(num1, num2);
+                    break;
+                case '*':
+                    result = multiplication(num1, num2);
+                    break;
+                default:
+                    System.out.println(operator + " is not supported.");
             }
-            System.out.println("DONE.");
 
+            System.out.println("Your calculation equals: " + result);
 
-
+            System.out.println("Do you want to continue (y/n)");
+            char test = scanner.next().charAt(0);
+            if (test == 'n') {
+                calculate = false;
+            } if (test != 'y' && test != 'n') {
+                System.out.println("Your choice is not valid." /*Please choose (y/n)."*/);
+                calculate = false;
             }
         }
+        System.out.println("Thank you for using our calculator.");
+    }
+}
 
 
 
